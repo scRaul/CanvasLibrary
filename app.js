e5c6fc2d5a0c49f5b122a022d9eb0cc2587a7766
scRaul <61263078+scRaul@users.noreply.github.com>
2022-12-10 10:30:50 -0800

    reworked functionality

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
+//MVP drawing app to test out canvas libray
+
 document.getElementById('clearButton').addEventListener('click',clearScreen);
 var pxSizeInput = document.getElementById('pxSize');
 var penColorInput = document.getElementById('penColor');
-var olMode = document.getElementById('olMode');
 var traceMode = document.getElementById('traceMode');
-var olButton = document.getElementById('olButton');
-olButton.style.display = 'none';
+var userImage = document.getElementById('userImage');
+
 pxSizeInput.addEventListener('change',setPenSize);
 penColorInput.addEventListener('change',setPenColor);
-olMode.addEventListener('change',toggleMode);
-olButton.addEventListener('click',generateOutline);
+traceMode.addEventListener('change',toggleMode);
+userImage.addEventListener('change',setImage);
+
+toggleMode();
 
 var increments = 500;
 var traceScreen = new Screen(500,500,increments);
 var screen = new Screen(500,500,increments);
 
-var testImg = new ScreenObj();
-testImg.setSize(increments,increments);
-testImg.setColor(new Color(0,0,0,0));
-testImg.addImageSrc("test.png");
+var traceImg = new ScreenObj();
+traceImg.setSize(increments,increments);
+traceImg.setColor(new Color(0,0,0,0));
+traceImg.addImageSrc("test.png");
 
 var input = new Input();
 var pen = new ScreenObj();
@@ -26,102 +29,56 @@ input.addScreen(screen);
 
 var pointList = [];
 var pointListB = [];
-
-
-
 const fpsDefault = 10; 
 const fpsMovement = 120;
 var fps = fpsDefault;
 
 setPenColor();
 updateAndRender();
+screen.drawLineGrid(10);
 async function updateAndRender(){
     var delay = 1000/fps;
     var startTime = Date.now();
-    handleInput();
-    if(!olMode.checked)
-        drawInput();
-    else
-        olInput();
+    drawInput();
     screen.render();
     if(traceMode.checked){
         traceScreen.show();
         traceScreen.render();
-        traceScreen.drawScreenObject(testImg);
+        traceScreen.draw([traceImg],DRAW_MODE.SCREENOBJ);
     }else{
         traceScreen.hide();
     }
     await new Promise(r => setTimeout(r,(startTime+delay)-Date.now() ));
     updateAndRender();
 }
-function handleInput(){
- if(input.keys.d == true){
-    input.keys.d = false;
-    drawPointGrid(5,screen,pen);
- }
-}
-var firstDown = true;
 function drawInput(){
     if(input.mouseDown == true){
        fps = fpsMovement;
-       pen.teleport(input.getMousePositon());
-       drawPointGrid(5,screen,pen);
-    //    screen.drawScreenObject(pen);
        var v = input.getMousePositon();
        v.z =1; v.w = 1;
        pointList.push(v);
        pointListB.push(v);
-       if( firstDown ){
-        pointList.push(v);
-        pointListB.push(v);
-        pointList.push(v);
-        pointListB.push(v);
-        pointList.push(v);
-        pointListB.push(v);
-        firstDown = false;
-       }
+       screen.draw([v],DRAW_MODE.POINT,pen.getWidth(),pen.getColor());
        if(pointList.length >11){
-            screen.drawBSpline(pointList,pen.getColor(),pen);
-            v = pointList[[pointList.length-1]];
+            screen.draw(pointList,DRAW_MODE.LINE,pen.getWidth(),pen.getColor());
             pointList = [];
-            pointList.push(v);
-            pointList.push(v);
-            pointList.push(v);
        }
        if(pointListB.length >99){
-            screen.drawBSpline(pointListB,pen.getColor(),pen);
-            v = pointList[[pointList.length-1]];
+            screen.draw(pointListB,DRAW_MODE.LINE,pen.getWidth(),pen.getColor());
             pointListB = [];
-            pointListB.push(v);
-            pointListB.push(v);
-            pointListB.push(v);
        }
-    }else{
-        firstDown = true;
+    }
+    else{
+        screen.draw(pointListB,DRAW_MODE.LINE,pen.getWidth(),pen.getColor());
         fps = fpsDefault;
         pointList = [];
         pointListB = [];
     }
 }
-function olInput(){
-    if(input.mouseDown == true){
-        input.mouseDown = false;
-
-        pen.teleport(input.getMousePositon());
-        screen.drawScreenObject(pen);
-        var v = input.getMousePositon();
-        v.z =1; v.w = 1;
-        
-        pointListB.push(v);
-        if(firstDown == true ){
-            pointListB.push(v);
-            pointListB.push(v);
-            pointListB.push(v);
-        }
-    }
-}
 function clearScreen(){
     screen.clearColor();
+    pointList = [];
+    pointListB = [];
 }
 function setPenSize(){
     var size = pxSizeInput.value;
@@ -134,22 +91,14 @@ function setPenColor(){
     const b = parseInt(color.substr(5,2), 16)
     pen.setColor(new Color(r,g,b));
 }
-
 function toggleMode(){
-    if(olMode.checked){
-        olButton.style.display = 'inline-block';
+    if(traceMode.checked){
+        userImage.style.display = 'inline-block';
     }
     else{
-        olButton.style.display = 'none';
+        userImage.style.display = 'none';
     }
 }
-function generateOutline(){
-    firstDown = false;
-    pointListB.push(pointListB[pointListB.length-1]);
-    pointListB.push(pointListB[pointListB.length-1]);
-    pointListB.push(pointListB[pointListB.length-1]);
-    if(pointListB.length > 0){
-        screen.drawBSpline(pointListB,pen.getColor(),pen);
-        pointListB = [];
-   }
+function setImage(){
+
 }
