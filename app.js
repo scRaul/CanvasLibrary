eccaa58a9c268af5bb4b4409fab9a494f65c7a00
scRaul <61263078+scRaul@users.noreply.github.com>
2022-12-11 15:07:04 -0800

    Added Functionality to ScreenObj, now can be parented

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var pxSizeInput = document.getElementById('pxSize');
 var penColorInput = document.getElementById('penColor');
 var traceMode = document.getElementById('traceMode');
 var userImage = document.getElementById('userImage');
+var instruc = document.getElementById("imageInst");
 var gridMode = document.getElementById('gridMode');
 var funcMode = document.getElementById('functionMode');
 var bottmCntrls = document.getElementById('bottomCntrls');
-
 // set up event listeners 
 pxSizeInput.addEventListener('change',setPenSize);
 penColorInput.addEventListener('change',setPenColor);
@@ -17,8 +17,6 @@ traceMode.addEventListener('change',toggleMode);
 gridMode.addEventListener('change',toggleMode);
 userImage.addEventListener('change',setImage);
 funcMode.addEventListener('change',toggleMode);
-
-
 // set up 3 screens, one main for drawing, 1 for displaying a background image, as a grid
 var increments = 500;
 var traceScreen = new Screen(500,500,increments,'canvas3');
@@ -33,11 +31,9 @@ var screen = new Screen(500,500,increments,'canvas');
 // gridScreen.render();
 // // hidden util checkbox clicked 
 // gridScreen.hide();
-
-// create an object, this is where the background image will be 
 var traceImg = new ScreenObj();
 // scale image to size of grid 
-traceImg.setSize(increments/10,increments/10);
+traceImg.setSize(increments/4,increments/4);
 // make object tranparent 
 traceImg.setColor(new Color(0,0,0,0));
 // add and image to the object
@@ -52,63 +48,92 @@ input.setScreen(screen);//detect when mouse interact with screen
 // will keep track of points where the use touched
 var pointList = [];
 var pointListB = [];//back up list 
+var pointListC = [];
 
 const fpsDefault = 10; // used to slow down the update the upadate and render loop
 const fpsMovement = 120; // used to speed up the update for loop
 var fps = fpsDefault;
 screen.clearColor(); // set default clear color 
+traceMode.checked = true;
 
 setPenColor(); // check color 
 toggleMode(); // check checkBoxes
 updateAndRender(); // begin 
-var deg = 0;
 async function updateAndRender(){
     var delay = 1000/fps;
     var startTime = Date.now();
+    handleKeyInput();
     drawInput();
     screen.render();
     traceScreen.render();
     if(traceMode.checked){
+        //show vertices around the image 
          traceScreen.draw([traceImg],DRAW_MODE.POINT,10,new Color(255));
-         traceScreen.draw([traceImg],DRAW_MODE.SCREENOBJ,10,new Color(255));
+         //show the image itslef 
+         traceScreen.draw([traceImg],DRAW_MODE.SCREENOBJ);
     }
     await new Promise(r => setTimeout(r,(startTime+delay)-Date.now() ));
     updateAndRender();
 }
 function drawInput(){
-    if(input.keys.d==true){
-        input.keys.d = false;
-        console.log(input.keys.d)
-        traceScreen.clearColor(new Color(0,0,0,0));
-        traceImg.rotate(deg);
-        deg++;
-    }
     if(input.mouseDown == true){
         fps = fpsMovement;
         var v = input.getMousePositon(); 
         pen.teleport(v);
+        if(funcMode.checked) {
+         checkInput();
+         return;
+        }
         pointList.push(v); 
         pointListB.push(v);
-        if(funcMode.checked) 
-            checkInput();
+        pointListC.push(v);
         // draw a single point 
-        screen.draw([v],DRAW_MODE.POINT,pen.getWidth(),pen.getColor());
-       if(pointList.length >11){
+       screen.draw([v],DRAW_MODE.POINT,pen.getWidth(),pen.getColor());
+       if(pointList.length > 9){
         //draw a list of points as a line
             screen.draw(pointList,DRAW_MODE.LINE,pen.getWidth(),pen.getColor());
             pointList = [];
        }
-       if(pointListB.length >99){
+       if(pointListB.length > 1000){
             screen.draw(pointListB,DRAW_MODE.LINE,pen.getWidth(),pen.getColor());
             pointListB = [];
        }
     }
     else{
+        screen.draw(pointList,DRAW_MODE.LINE,pen.getWidth(),pen.getColor());
         screen.draw(pointListB,DRAW_MODE.LINE,pen.getWidth(),pen.getColor());
+        screen.draw(pointListC,DRAW_MODE.LINE,pen.getWidth(),pen.getColor());
+        pointListC = [];
         fps = fpsDefault;
-        pointList = [];
-        pointListB = [];
     }
+}
+var x = traceImg.getVerts()[0].x;
+var y = traceImg.getVerts()[0].y;
+function handleKeyInput(){
+    if(input.keyDown == false){
+        traceScreen.clearColor(new Color(0,0,0,0));
+        fps = fpsDefault;
+        return;
+    }
+    fps = fpsMovement;
+    //move around the trace image
+    if(input.keys.a){
+        traceScreen.clearColor();
+        x-=10;
+    }
+    else if(input.keys.d){
+        traceScreen.clearColor();
+        x+=10;
+    }
+    else if(input.keys.w){
+        traceScreen.clearColor();
+        y+=10;
+    }
+    else if(input.keys.s){
+        traceScreen.clearColor();
+        y-=10;
+    }
+    traceImg.teleport(new Vec(x,y));
 }
 function checkInput(){
     var e = document.getElementsByName('Shape');
@@ -164,9 +189,11 @@ function setPenColor(){
 function toggleMode(){
     if(traceMode.checked){
         userImage.style.display = 'inline-block';
+        instruc.style.display = 'block';
     }
     else{
         userImage.style.display = 'none';
+        instruc.style.display = 'none';
     }
     if(gridMode.checked){
        gridScreen.show();
